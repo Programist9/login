6b08404d6646e0c58f7ce2e11791d2e7e98a5f92
panda <pandachka874@gmail.com>
2024-03-13 19:31:15 +0200

    updated

--- login.py
+++ login.py
@@ -1,17 +1,20 @@
 import sys
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout
-
+# Клас для окна для логина
 class LoginWindow(QWidget):
     def __init__(window):
         super().__init__()
+        # Название для окна логина
         window.setWindowTitle("Login")
+        # Размеры логин окна
         window.resize(250,300)
-
+        # Основная вертикальная линия
         layout = QVBoxLayout()
-
+        # Юзернэйм текст создаётся
         window.username_label = QLabel("Username:")
         window.username_input = QLineEdit()
+        # Юзернэйм текст прикрипляется
         layout.addWidget(window.username_label, alignment=Qt.AlignCenter)
         layout.addWidget(window.username_input)
 
@@ -22,6 +25,7 @@ class LoginWindow(QWidget):
         layout.addWidget(window.password_input)
 
         window.login_button = QPushButton("Login")
+        # Кнопка
         window.login_button.clicked.connect(window.login)
         layout.addWidget(window.login_button)
 
